13748352d4eab29bb75feb0e1961b0197e792077
MZakharina <machm@mail.ru>
2025-04-26 17:07:07 +0300

    module1 task2

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+AUTHOR = {
+    'Имя': 'Иван',
+    'Отчество': 'Петрович',
+    'Фамилия': 'Иванов',
+    'телефон': '8-923-600-01-02',
+    'email': 'vasya@mail.ru'
+    }
+
+def home(request):
+    text = """
+    <h1>"Изучаем django"</h1>
+    <strong>Автор</strong>: <i>Иванов И.П.</i>
+    <br>
+    <href>About</href>
+
+    """
+    return HttpResponse(text)
+
+def about(request):
+    text = ''
+    for k, v in AUTHOR.items():
+        text += f'<i>{k}: {v}</i><br>'
+    return HttpResponse(text)
+
