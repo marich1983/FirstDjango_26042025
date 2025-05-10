e220ee6b4b54e614f8e4ab0debfcef42a293785c
MZakharina <machm@mail.ru>
2025-05-10 12:19:20 +0300

    Шаблон base.html применен к items_list

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('', views.home),
-    path('about', views.about),
-    path('items', views.items),
-    path('item/<int:item_id>', views.item_info)
+    path('', views.home, name="home"),
+    path('about', views.about, name="about"),
+    path('items', views.items, name="items-list"),
+    path('item/<int:item_id>', views.item_info, name="item-info")
 ]
